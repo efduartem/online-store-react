fcb8a7a1d6700905868297ceaf258778be1456f1
efduartem <efduartem@gmail.com>
2017-11-11 18:24:32 -0300

    Init Online Store with ReactJS

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class App extends React.Component{
+
+    constructor(){
+      super();
+    }
+
+    render(){
+        return(
+            <div>
+              <h1>Online Store Init</h1>
+            </div>
+        );
+    }
+
+}
+
+export default App;
